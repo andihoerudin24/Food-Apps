2a881cd9d3b933cf35a129c56c1c45cae1febc49
Andi Hoerduin <andihoerudin24@gmail.com>
2020-06-21 22:50:32 +0700

    end

--- components/ResultList.js
+++ components/ResultList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text, StyleSheet,FlatList,TouchableOpacity } from "react-native";
 import ResultDetail from './ResultDetail'
-
+ 
 const ResultList = props => {
   if(!props.results.length){
     return null
